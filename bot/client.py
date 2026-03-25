805f6a0110ef47dca92bc721ca2a450eeab330d2
Aryan <aryankatiyarjnv@gmail.com>
2026-03-25 17:58:37 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -2,18 +2,14 @@ import os
 from binance.client import Client
 from dotenv import load_dotenv
 
+load_dotenv()
 
-def get_client():
-    # Load environment variables from .env
-    load_dotenv()
-
+def get_binance_client():
     api_key = os.getenv("BINANCE_API_KEY")
     api_secret = os.getenv("BINANCE_API_SECRET")
 
     if not api_key or not api_secret:
-        raise ValueError("API keys not found. Please check your .env file.")
+        raise ValueError("API keys not found. Check your .env file.")
 
-    # Initialize Binance client for Futures Testnet
     client = Client(api_key, api_secret, testnet=True)
-
     return client
